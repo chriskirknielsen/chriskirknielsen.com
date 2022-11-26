a1397ba57ae29ab32294666cbdcedfc4fbb219ab
chriskirknielsen <chriskirknielsen@gmail.com>
2022-11-26 14:08:13 -0500

    Clean up the asset pipeline code

--- .eleventy.js
+++ .eleventy.js
@@ -9,8 +9,8 @@ const defaultLang = 'en';
 // Tools
 const util = require('util');
 const path = require('node:path');
-const glob = require('glob');
 const fs = require('fs');
+const glob = require('glob');
 const jsonSass = require('json-sass');
 const deepmerge = require('deepmerge');
 const { PurgeCSS } = require('purgecss');
@@ -111,19 +111,25 @@ module.exports = function (eleventyConfig) {
 		/**
 		 * Compile a list of files from the src/assets folder to src/_includes/assets.
 		 * @param {Object} settings Configuration for the compiler.
-		 * @param {string} settings.inFolder
-		 * @param {string} settings.inExt
-		 * @param {string} settings.outFolder
-		 * @param {string} [settings.outExt]
-		 * @param {function} [settings.filterFn]
-		 * @param {function} settings.compileFn
+		 * @param {string} settings.inFolder Name of the input folder.
+		 * @param {string} settings.inExt Extension of the input files.
+		 * @param {string} [settings.outFolder] Optional. Name of the output folder. Defaults to the same name as `inFolder`.
+		 * @param {string} [settings.outExt] Optional. Extension of the output files. Defaults to the same extension as `inExt`.
+		 * @param {function} [settings.filterFn] Optional. Function run against the list of file paths retuning a boolean describing if the file should be compiled.
+		 * @param {function} settings.compileFn Compiler for the provided files.
 		 * @returns {Promise<string[]>} List of output files.
 		 */
 		const compileAssets = (settings) => {
 			const _requiredSettings = ['inFolder', 'inExt', 'compileFn'];
+
 			// Check all the correct data is passed
 			if (_requiredSettings.some((s) => !settings.hasOwnProperty(s))) {
 				throw 'The settings object is missing required properties: '.concat(_requiredSettings.filter((s) => !settings.hasOwnProperty(s)).join(', '));
+			}
+
+			// Ensure the compile function is properly provided
+			if (typeof settings.compileFn !== 'function') {
+				throw 'The compileFn property must be a function that takes a parsed path object as an argument.';
 			}
 
 			// If the output extension is missing, use the input extension
@@ -139,7 +145,7 @@ module.exports = function (eleventyConfig) {
 			// Create a promise so we can mark it as resolved when all the files are compiled
 			return new Promise((resolve, reject) => {
 				// Grab a list of all the files matching the folder and extension
-				const inputFolder = `${rootDir}/assets/${settings.inFolder}`;
+				const inputFolder = `${config.inputDir}/assets/${settings.inFolder}`;
 				return glob(`${inputFolder}/**/*.${settings.inExt}`, './', (globError, inputFiles) => {
 					if (globError) {
 						return reject(globError);
@@ -153,7 +159,7 @@ module.exports = function (eleventyConfig) {
 						// Get the parsed path for the file
 						const parsed = path.parse(inputPath);
 
-						// Compute the output folder name, taking whatever is after the base asset folder and removingleading and trailing slashes
+						// Compute the output folder name, taking whatever is after the base asset folder and removing leading and trailing slashes
 						const outputFolder = parsed.dir
 							.split(inputFolder)
 							.pop()
@@ -162,7 +168,8 @@ module.exports = function (eleventyConfig) {
 							.trim();
 
 						// Compute the final output folder, checking if the output folder has a value to append a slash if needed
-						const folder = `${rootDir}/_includes/assets/${settings.outFolder}/${outputFolder.length > 0 ? outputFolder + '/' : ''}`;
+						const subfolder = outputFolder.length > 0 ? `${outputFolder}/` : '';
+						const folder = `${config.inputDir}/_includes/assets/${settings.outFolder}/${subfolder}`;
 
 						// Compute the final path with the file name
 						const outputPath = `${folder}${parsed.name}.${settings.outExt}`;
@@ -172,16 +179,16 @@ module.exports = function (eleventyConfig) {
 
 						// Return a promise that handles generating the target output file
 						return new Promise((success, failure) =>
-							// Create the folder structure if it doesn't exist
-							fs.mkdir('./' + folder, { recursive: true }, (err, path) => {
-								if (err) {
-									return failure(err);
+							// Create the folder structure if it doesn't exist, including subfolders thanks to `recursirve: true`
+							fs.mkdir('./' + folder, { recursive: true }, (dirErr, path) => {
+								if (dirErr) {
+									return failure(dirErr);
 								}
 
 								// Write the file with the provided result
-								fs.writeFile(outputPath, result, { flag: 'w' }, (error) => {
-									if (error) {
-										return failure(error);
+								fs.writeFile(outputPath, result, { flag: 'w' }, (fileError) => {
+									if (fileError) {
+										return failure(fileError);
 									}
 									success(outputPath);
 								});
@@ -189,17 +196,23 @@ module.exports = function (eleventyConfig) {
 						);
 					});
 
+					// Resolve the `compileAssets` promise when all the files are processed
 					Promise.all(compiledFiles).then((savedFiles) => resolve(savedFiles));
 				});
 			});
 		};
 
 		// Compile the JSON tokens file to a Sass file first
-		const tokens = Promise.resolve(
+		const tokens = new Promise((resolve, reject) =>
 			fs
-				.createReadStream(`${rootDir}/_data/tokens.json`)
+				.createReadStream(`${config.inputDir}/_data/tokens.json`)
 				.pipe(jsonSass({ prefix: '$tokens: ' }))
-				.pipe(fs.createWriteStream(`${rootDir}/assets/scss/tools/_tokens.scss`))
+				.pipe(
+					fs
+						.createWriteStream(`${config.inputDir}/assets/scss/tools/_tokens.scss`)
+						.on('finish', () => resolve())
+						.on('error', () => reject())
+				)
 		);
 
 		const styles = compileAssets({
@@ -207,7 +220,7 @@ module.exports = function (eleventyConfig) {
 			inExt: 'scss',
 			outFolder: 'css',
 			outExt: 'css',
-			filterFn: (inputPath) => !inputPath.startsWith(`${rootDir}/assets/scss`) || !inputPath.split('/').pop().startsWith('_'),
+			filterFn: (inputPath) => !inputPath.split('/').pop().startsWith('_'),
 			compileFn: async (parsed) => {
 				const result = sass.compile(`${parsed.dir}/${parsed.base}`, {
 					loadPaths: [parsed.dir || '.', config.dir.includes],
@@ -242,8 +255,11 @@ module.exports = function (eleventyConfig) {
 		templateFormats: ['md', 'html', 'njk'],
 		preAttributes: {
 			tabindex: 0,
-			class: (context) => `language-${context.language} codeblock content-wide`,
-			'data-lang': (context) => context.language.toUpperCase(),
+			class: (context) => {
+				console.log(context);
+				return `${context.language ? 'language-' + context.language : ''} codeblock content-wide`.trim();
+			},
+			'data-lang': (context) => (context.language ? context.language.toUpperCase() : ''),
 		},
 	});
 	eleventyConfig.addPlugin(EleventyRenderPlugin);
@@ -524,10 +540,7 @@ module.exports = function (eleventyConfig) {
 	});
 
 	/* Watch targets */
-	eleventyConfig.addWatchTarget(`./${rootDir}/assets/`);
-	eleventyConfig.addWatchTarget(`./${rootDir}/assets/scss/`);
 	eleventyConfig.addWatchTarget(`./${rootDir}/assets/scss/**/*.scss`);
-	eleventyConfig.addWatchTarget(`./${rootDir}/assets/js/`);
 	eleventyConfig.addWatchTarget(`./${rootDir}/assets/js/**/*.js`);
 
 	eleventyConfig.watchIgnores.add(`./${rootDir}/assets/scss/tools/_tokens.scss`);
