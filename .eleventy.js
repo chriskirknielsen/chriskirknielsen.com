0d77337c203cc92402bc8fbb7fe7598c21262e93
chriskirknielsen <chriskirknielsen@gmail.com>
2023-01-02 15:51:49 -0500

    [fix] Card cover link style for fonts

--- .eleventy.js
+++ .eleventy.js
@@ -199,6 +199,7 @@ module.exports = function (eleventyConfig) {
 		locale = locale || context.lang || defaultLang;
 		return collection.filter((item) => item?.data?.lang === locale);
 	});
+	eleventyConfig.addFilter('removeTrailingSlash', (str) => str.trim().replace(/\/$/g, ''));
 	eleventyConfig.addFilter('stripHyphenChars', (str) => str.replace(/(&shy;|<wbr>)/gm, ''));
 	eleventyConfig.addFilter('stripScripts', (str) => str.replace(/(<script( [^>]+)?>(.+)<\/script>)/gim, '')); // Not 100% reliable, however I don't usually add `</script>` inside a script tag so it should be safe!
 	eleventyConfig.addFilter('toLowercase', (str) => str.toLowerCase());
