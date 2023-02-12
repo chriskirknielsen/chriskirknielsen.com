946773c07283b828da6337127199e3dd24009183
chriskirknielsen <chriskirknielsen@gmail.com>
2023-02-12 18:09:31 -0500

    Ensure Projects are also excluded as system tags

--- src/_data/systemtags.js
+++ src/_data/systemtags.js
@@ -1,6 +1,6 @@
 const locales = Object.keys(require('./metadata.js').locales);
 locales.push('all'); // We also want to filter out collections for all languages
-const systemtags = ['all', 'post', 'page', 'designs', 'fonts'];
+const systemtags = ['all', 'post', 'page', 'designs', 'fonts', 'projects'];
 for (let locale of locales) {
 	systemtags.push(`page_${locale}`);
 	systemtags.push(`post_${locale}`);
