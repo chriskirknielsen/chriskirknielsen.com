42d5f25dcac01e3c2ec20c50088561d61d32aa31
chriskirknielsen <chriskirknielsen@gmail.com>
2023-04-01 15:47:35 -0400

    [feature] Improve the DVD screensaver feature with confetti on corner bounce

--- src/assets/js/screensaver.js
+++ src/assets/js/screensaver.js
@@ -8,17 +8,67 @@
 	// List available colors
 	const colors = ['tomato', 'green', 'slateblue', 'yellow', 'cyan', 'orange', 'deeppink', 'white'];
 	let lastColor;
+	let lastCollisionTime;
 
 	const getRandomItem = (list) => list[Math.floor(Math.random() * list.length)];
 	const setScreenSaver = (isOn = false) => {
 		// Find an existing screensaver…
 		let screenSaver = document.querySelector('.screensaver');
 
+		// Define the "collision" function
+		const onCollision = (e) => {
+			if (!['scrsvrX', 'scrsvrY'].includes(e.animationName)) {
+				return;
+			}
+			const diffColors = colors.filter((c) => c !== lastColor); // Exclude last used color
+			const newCollisionTime = Date.now();
+			const isDoubleCollision = newCollisionTime - lastCollisionTime <= 100; // If two collisions occurred nearly at the same time, we can pretent it's a corner hit, party!
+			const newColor = isDoubleCollision ? lastColor : getRandomItem(diffColors);
+
+			if (isDoubleCollision) {
+				const confettiCount = 128;
+				const confettiRemove = (e) => {
+					const confetti = e.target;
+					if (e.animationName !== 'scrsvrConfettiTransform') {
+						return;
+					}
+					confetti.removeEventListener('animationend', confettiRemove);
+					confetti.remove();
+				};
+
+				for (let c = 0; c < confettiCount; c++) {
+					const confetti = document.createElement('div');
+					const color = getRandomItem(colors);
+					const speed = 1 + parseFloat((Math.random() / 5 - 0.1).toFixed(3)); // Value between 0.9 and 1.1
+					const delay = Math.round(Math.random() * 500);
+					const scale = (Math.random() / 2 + 0.5).toFixed(3);
+					const rotate = parseFloat((Math.random() - 0.5).toFixed(3));
+					const translate = ((c / confettiCount) * 100 + (Math.random() - 0.5)).toFixed(3);
+					confetti.classList.add('confetti');
+					confetti.setAttribute('aria-hidden', 'true');
+					confetti.style.setProperty('--confetti-color', color);
+					confetti.style.setProperty('--confetti-speed', speed);
+					confetti.style.setProperty('--confetti-delay', delay);
+					confetti.style.setProperty('--confetti-scale', scale);
+					confetti.style.setProperty('--confetti-rotate', rotate);
+					confetti.style.setProperty('--confetti-translate', translate);
+					screenSaver.appendChild(confetti);
+					confetti.addEventListener('animationend', confettiRemove);
+				}
+			}
+
+			screenSaver.style.setProperty('--scrnsvr-logo-fill', newColor);
+			lastColor = newColor;
+			lastCollisionTime = newCollisionTime;
+		};
+
 		// If there is a screensaver, remove it if required, or leave it if it should be visible
-		if (screenSaver && !isOn) {
-			screenSaver.remove();
-		}
 		if (!isOn) {
+			if (screenSaver) {
+				screenSaver.removeEventListener('animationiteration', onCollision);
+				screenSaver.remove();
+			}
+
 			document.documentElement.removeAttribute('data-screensaver');
 			return;
 		}
@@ -39,14 +89,9 @@
 		screenSaver.classList.add('screensaver');
 
 		// Change color once a border is hit
-		screenSaver.addEventListener('animationiteration', (e) => {
-			const diffColors = colors.filter((c) => c !== lastColor); // Exclude last used color
-			const newColor = getRandomItem(diffColors);
-			screenSaver.style.setProperty('--scrnsvr-logo-fill', newColor);
-			lastColor = newColor;
-		});
+		screenSaver.addEventListener('animationiteration', onCollision);
 
-		screenSaver.innerHTML = '<div><svg viewBox="0 0 159 64.8" width="159" height="65"><use href="#ckn-logo" /></svg></div>';
+		screenSaver.innerHTML = '<div><svg width="183.79" height="80.86" viewBox="0 0 183.79 80.86"><use href="#dvd-logo" /></svg></div>';
 		document.body.appendChild(screenSaver);
 		document.documentElement.setAttribute('data-screensaver', '');
 	};
@@ -54,7 +99,7 @@
 	document.addEventListener('keyup', (e) => {
 		if (e.isTrusted) {
 			if (keys.konami[keys.index] === e.key) {
-				if (keys.index == 9) {
+				if (keys.index == keys.konami.length - 1) {
 					keys.index = 0;
 
 					setScreenSaver(true);
