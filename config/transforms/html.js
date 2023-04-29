b2e2f3cfc035271259823a75013fe6c891ee51b8
chriskirknielsen <chriskirknielsen@gmail.com>
2023-04-29 18:45:44 -0400

    [style] Make the hreflang tip in CSS content instead

--- config/transforms/html.js
+++ config/transforms/html.js
@@ -12,33 +12,6 @@ module.exports = function (eleventyConfig, options = {}) {
 		options
 	);
 
-	/** Attach a language indicator. */
-	eleventyConfig.addTransform('hreflang', function (content, outputPath) {
-		if (!outputPath.endsWith('.html')) {
-			return content;
-		}
-
-		const $ = cheerio.load(content, null, true); // Load the contents into cheerio to get a DOM representation
-		const contentLang = $('html').attr('lang');
-
-		// If the page is already in English (default lang), return the content as is
-		if (contentLang === 'en') {
-			console.log('lang=en', outputPath);
-			return content;
-		}
-
-		const hreflangLinks = $('a[href][hreflang]'); // Find all hreflang references
-		hreflangLinks.each(function () {
-			const link = $(this);
-			const targetLang = link.attr('hreflang');
-			if (contentLang !== targetLang) {
-				link.append($(`<span class="visually-hidden hreflang-tip">(${targetLang})</span>`));
-			}
-		});
-
-		return $.html();
-	});
-
 	/** Add ability to minify inline markup. Also useful to bypass Markdown parsing. */
 	eleventyConfig.addFilter('htmlmin', function (content) {
 		return htmlmin.minify(content, minifyOpts);
