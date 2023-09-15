8b1d028db969867e9648091556543ae1f633fe5b
chriskirknielsen <chriskirknielsen@gmail.com>
2023-09-15 19:26:18 -0400

    [content] Reorganise social links order in footer

--- src/_data/social.js
+++ src/_data/social.js
@@ -10,18 +10,16 @@ module.exports = [
 		url: 'https://mastodon.social/@chriskirknielsen',
 		showOnHome: true,
 	},
-
-	{
-		key: 'github',
-		name: 'GitHub',
-		url: 'https://github.com/chriskirknielsen',
-		showOnHome: false,
-	},
-
 	{
 		key: 'codepen',
 		name: 'CodePen',
 		url: 'https://codepen.io/chriskirknielsen',
 		showOnHome: true,
+	},
+	{
+		key: 'github',
+		name: 'GitHub',
+		url: 'https://github.com/chriskirknielsen',
+		showOnHome: false,
 	},
 ];
