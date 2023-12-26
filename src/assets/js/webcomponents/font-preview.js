e16cbf5e1fe696b6aed2a6ed880485b7466ffc00
chriskirknielsen <chriskirknielsen@gmail.com>
2023-12-26 13:42:31 -0500

    [dev] Add a few comments to Type Tester

--- src/assets/js/webcomponents/font-preview.js
+++ src/assets/js/webcomponents/font-preview.js
@@ -2,24 +2,28 @@ class FontPreview extends HTMLElement {
 	constructor() {
 		super();
 
+		// Grab all interactive elements
 		this.fontPreviewInput = this.querySelector('[data-fontpreview="preview-input"]');
 		this.fontPreviewOutputWrap = this.querySelector('[data-fontpreview="preview-output-wrap"]');
 		this.fontPreviewOutput = this.querySelector('[data-fontpreview="preview-output"]');
 		this.fontSpecimenSampleDefault = this.getAttribute('default-sample') || 'Sample Text';
 
+		// Listen for events
 		this.addEventListener('click', this);
 		this.addEventListener('keyup', this);
 		this.addEventListener('change', this);
 		this.addEventListener('submit', this);
 	}
 
-	updateFontPreviewText = (e = null) => {
+	// When a user changes the custom text to preview
+	updateFontPreviewText = () => {
 		const text = this.fontPreviewInput.value.trim() || this.fontSpecimenSampleDefault;
 		this.fontPreviewOutputWrap.setAttribute('aria-label', text);
 		this.fontPreviewOutput.textContent = text;
 	};
 
-	updateFontPreviewOptions = (e = null) => {
+	// When a user changes the font options
+	updateFontPreviewOptions = () => {
 		let previewOptions = Array.from(this.querySelectorAll('[data-preview-option]'));
 		previewOptions.forEach((previewOption) => {
 			let classToToggle = previewOption.getAttribute('name');
@@ -33,18 +37,18 @@ class FontPreview extends HTMLElement {
 		switch (e.type) {
 			case 'click': {
 				if (e.target.closest('[data-fontpreview="preview-update"]')) {
-					this.updateFontPreviewText(e);
-					this.updateFontPreviewOptions(e);
+					this.updateFontPreviewText();
+					this.updateFontPreviewOptions();
 				}
 				break;
 			}
 			case 'keyup':
 			case 'submit': {
-				this.updateFontPreviewText(e);
+				this.updateFontPreviewText();
 				break;
 			}
 			case 'change': {
-				this.updateFontPreviewOptions(e);
+				this.updateFontPreviewOptions();
 				break;
 			}
 		}
@@ -56,7 +60,7 @@ class FontPreview extends HTMLElement {
 
 	connectedCallback() {
 		// Once defined, we can run initial logic
-		this.classList.add('defined');
+		this.classList.add('defined'); // Workaround for browsers that support web components but not the :defined CSS pseudo-class
 		this.updateFontPreviewText();
 		this.updateFontPreviewOptions();
 	}
